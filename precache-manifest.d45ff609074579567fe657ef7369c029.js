052989ab9288496f2edd1535870eec6ff14c075a
psdevorder <dejan.barlek@gmail.com>
2020-10-06 18:23:39 +0200

    Updates

---
+++ precache-manifest.d45ff609074579567fe657ef7369c029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10868a0bb81edf783065f853dec7db99",
+    "url": "/reactreminder/index.html"
+  },
+  {
+    "revision": "b80dce79419e6166c5cb",
+    "url": "/reactreminder/static/css/main.4b94a3e8.chunk.css"
+  },
+  {
+    "revision": "f667041e6b08e5f98714",
+    "url": "/reactreminder/static/js/2.dd6b4795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactreminder/static/js/2.dd6b4795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80dce79419e6166c5cb",
+    "url": "/reactreminder/static/js/main.03efc56a.chunk.js"
+  },
+  {
+    "revision": "f50cf70d41f91ab4cc48",
+    "url": "/reactreminder/static/js/runtime-main.5624384c.js"
+  }
+]);
