d8f601a8307df959df91d82fa9d15406c563009d
psdevorder <dejan.barlek@gmail.com>
2020-10-06 15:34:30 +0200

    added editing feature

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Card({ reminders, reminder, setReminders }) {
+  const [editMode, setEditMode] = useState(false);
+  const [imePolje, setImePolje] = useState("");
+  const [reminderPolje, setReminderPolje] = useState("");
+
   const handleDelete = () => {
     const filteredReminders = reminders.filter(
       (item) => reminder.id !== item.id
@@ -9,23 +13,76 @@ export default function Card({ reminders, reminder, setReminders }) {
   };
 
   const handleEdit = () => {
-    const filteredReminders = reminders.filter(
-      (item) => reminder.id !== item.id
-    );
-    setReminders(filteredReminders);
+    setEditMode(true);
+    setImePolje(reminder.ime);
+    setReminderPolje(reminder.remindTo);
   };
+
+  function hendlajSubmit(e) {
+    const editedReminder = {
+      id: reminder.id,
+      ime: imePolje.trim(),
+      remindTo: reminderPolje.trim(),
+      datum: reminder.datum,
+    };
+    if (
+      imePolje === "" ||
+      reminderPolje === "" ||
+      imePolje == null ||
+      reminderPolje == null
+    ) {
+      return e.preventDefault();
+    } else
+      return (
+        e.preventDefault(),
+        setReminders([
+          ...reminders.filter((item) => reminder.id !== item.id),
+          editedReminder,
+        ]),
+        setImePolje(""),
+        setReminderPolje(""),
+        setEditMode(false)
+      );
+  }
 
   return (
     <>
       <li>
-        <h4>{reminder.ime}</h4> <p className="taskText">{reminder.remindTo}</p>{" "}
-        <p className="datum">{reminder.datum}</p>
-        <button className="deleteBtn" onClick={handleDelete}>
-          Delete
-        </button>
-        <button className="editBtn" onClick={handleEdit}>
-          - Edit -
-        </button>
+        {/* {editMode ? <p>KE</p> : "BLČ"} */}
+        {editMode ? (
+          <form onSubmit={hendlajSubmit}>
+            <label htmlFor="person">Remind</label>
+            <input
+              type="text"
+              name="person"
+              id="person"
+              value={imePolje}
+              size="10"
+              onChange={(e) => setImePolje(e.target.value)}
+            />
+            <label htmlFor="reminder">to</label>
+            <input
+              type="text"
+              name="reminder"
+              id="reminder"
+              value={reminderPolje}
+              onChange={(e) => setReminderPolje(e.target.value)}
+            />
+            <input type="submit" style={{ display: "none" }} />
+          </form>
+        ) : (
+          <>
+            <h4>{reminder.ime}</h4>
+            <p className="taskText">{reminder.remindTo}</p>
+            <p className="datum">{reminder.datum}</p>
+            <button className="deleteBtn" onClick={handleDelete}>
+              Delete
+            </button>
+            <button className="editBtn" onClick={handleEdit}>
+              - Edit -
+            </button>
+          </>
+        )}
       </li>
     </>
   );
